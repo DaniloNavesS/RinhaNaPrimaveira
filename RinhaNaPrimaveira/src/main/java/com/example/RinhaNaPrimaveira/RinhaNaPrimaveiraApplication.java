e0e334d65678df0f43aed0feee2d545348db02ca
unknown <danilonaves2209@gmail.com>
2024-02-14 17:42:55 -0300

    Java init

---
+++ RinhaNaPrimaveira/src/main/java/com/example/RinhaNaPrimaveira/RinhaNaPrimaveiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.RinhaNaPrimaveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaNaPrimaveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaNaPrimaveiraApplication.class, args);
+	}
+
+}
---
+++ RinhaNaPrimaveira/src/test/java/com/example/RinhaNaPrimaveira/RinhaNaPrimaveiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RinhaNaPrimaveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaNaPrimaveiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
